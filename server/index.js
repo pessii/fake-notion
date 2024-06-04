6b7bc1d40869e9619f3380ab355fcad4ed5af64e
kpt_suzuki <868802338618>
2024-06-05 01:21:47 +0900

    ユーザー新規登録API実装

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@
 const express = require("express");
 //　mongooseライブラリをインポート
 const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
+const User = require("./src/v1/models/user");
 // express()を使って新しいアプリケーションを作成、appという変数に代入
 const app = express();
 //ローカルサーバーを起動するポート番号として5000を設定
@@ -20,6 +23,27 @@ try{
 }
 
 // ユーザー新規登録API
+app.post("/register", async (req, res) => {
+    // パスワードの受け取り
+    const password = req.body.password;
+
+    try {
+        // パスワードの暗号化
+        req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+
+        // ユーザーの新規作成
+        const user = await User.create(req.body);
+
+        // JWTの発行
+        const token = JWT.sign({id: user._id}, process.env.TOKEN_SECRET_KEY, {
+            expiresIn: "24h",
+        });
+        return res.status(200).json({user, token});
+
+    } catch(err) {
+        return res.status(500).json(err);
+    }
+});
 
 // ユーザーログイン用API
 
